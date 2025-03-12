7f02d9c95758790c0731ebac5fc4634e0a2e814a
Nathalia <karolgalvis10@gmail.com>
2025-03-12 16:52:58 -0500

    Archivo para variables

---
+++ src/services/messages.js
@@ -0,0 +1,4 @@
+export const WELCOME_MESSAGE = (name) =>` 👋 ¡Hola ${name}! Soy el asistente virtual de ESTILO PERFECTO 💇🏻‍♀️ tu peluqueria de confianza. 🚀\n\n¿En qué puedo ayudarte?`;
+
+export const MAIN_MENU_MESSAGE = `Para seleccionar una opción, simplemente escribe el número correspondiente:\n\n1️⃣ Consulta capilar \n2️⃣ Reservar cita \n3️⃣ Contacto y ubicación \n4️⃣ Contacta un Asesor 🆘`;
+
